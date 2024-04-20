d7359d1c02d10f9e465f6e5ad2eecefd07b5b13f
Nicholas Kang <nicholaskang5@gmail.com>
2024-04-19 23:38:02 -0700

    cubes go forward and back animation

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,10 +3,12 @@ import { useRef } from 'react'
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef() // remember: updating useRef() does not cause re-render, and useRef() val stays even when rerender happens
-  
+
   useFrame((state, delta) => { // delta is time since last animation frame (diff in time btwn curr and last frame)
     ref.current.rotation.y += delta
     ref.current.rotation.x += delta
+    ref.current.position.z += Math.sin(state.clock.elapsedTime) * 2 // remember, sin's range is [-1, 1]
+    // console.log(state.clock.elapsedTime)
   })
 
   return (

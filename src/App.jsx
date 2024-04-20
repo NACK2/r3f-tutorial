0dfd48280c9bd65192af950948f3b0425d643f96
Nicholas Kang <nicholaskang5@gmail.com>
2024-04-19 22:29:54 -0700

    changed size of box

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './App.css'
 
 const App = () => {
   return (
-      <Canvas>
-        <mesh>
-          <boxGeometry/>
-        </mesh>
-      </Canvas>
+    <Canvas>
+      <mesh> {/* mesh is building block for 3d objects */}
+        <boxGeometry args={[2, 2, 4]}/> {/* 2x2x4 3d box but looks like square because we are looking from z axis */}
+        <meshBasicMaterial />
+      </mesh>
+    </Canvas>
   )
 }
 

6c783cdd05b7de730b599a8e3bbc7b908b3d9a49
Nicholas Kang <nicholaskang5@gmail.com>
2024-04-19 22:39:51 -0700

    multiple cubes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import './App.css'
 const App = () => {
   return (
     <Canvas>
-      <directionalLight position={[-2, 0, 10]}/> 
+      <directionalLight position={[-2, 0, 5]}/> 
       <mesh position={[2, 0, 0]}> {/* mesh is building block for 3d objects */}
         <boxGeometry args={[1, 1, 1]}/> {/* args are x y z dimension size of box */}
+        <meshStandardMaterial color={"orange"}/>
+      </mesh>
+
+      <mesh position={[-2, 0, 0]}> 
+        <boxGeometry args={[1, 1, 1]}/> 
+        <meshStandardMaterial color={"orange"}/>
+      </mesh>
+
+      <mesh position={[0, 2, 0]}> 
+        <boxGeometry args={[1, 1, 1]}/> 
+        <meshStandardMaterial color={"orange"}/>
+      </mesh>
+
+      <mesh position={[0, -2, 0]}> 
+        <boxGeometry args={[1, 1, 1]}/> 
         <meshStandardMaterial color={"orange"}/>
       </mesh>
     </Canvas>

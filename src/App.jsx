f2a9e73366a2699ed99efc910707610f4b52b85a
Nicholas Kang <nicholaskang5@gmail.com>
2024-04-20 19:59:02 -0700

    added torusknot

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
 import Cube from './components/Cube'
 import Sphere from './components/Sphere'
+import TorusKnot from './components/TorusKnot'
 import './App.css'
 
 const App = () => {
@@ -12,13 +13,13 @@ const App = () => {
   return (
     <Canvas>
       <directionalLight position={[0, 0, 10]} intensity={1} />
-      <PerspectiveCamera position={[0, 0, -2]}>
+      <PerspectiveCamera position={[0, 0, -3]}>
         {/* <ambientLight intensity={0.3} /> ambient light illuminates every side equally */}
         <group position={[0, 0, 0]}>
           <Cube position={[2, 2, 0]} size={[1, 1, 1]} color='red' />
-          <Sphere position={[2, -2, 0]} size={[1, 30, 30]} color='green' />
-          <Cube position={[-2, 2, 0]} size={[1, 1, 1]} color='hotpink' />
-          <Cube position={[-2, -2, 0]} size={[1, 1, 1]} color='orange' />
+          <Sphere position={[2, -2, 0]} size={[1, 30, 30]} color='hotpink' />
+          <Cube position={[-2, 2, 0]} size={[1, 1, 1]} color='yellow' />
+          <TorusKnot position={[-2, -2, 0]} size={[1, 0.2, 100]} color='skyblue' />
         </group>
       </PerspectiveCamera>
     </Canvas>
--- src/components/TorusKnot.jsx
+++ src/components/TorusKnot.jsx
@@ -1,7 +1,7 @@
 import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 
-const Sphere = ({ position, size, color }) => {
+const TorusKnot = ({ position, size, color }) => {
   const ref = useRef()
 
   useFrame((state, delta) => {
@@ -12,10 +12,10 @@ const Sphere = ({ position, size, color }) => {
 
   return (
     <mesh position={position} ref={ref}>
-      <sphereGeometry args={size} />
+      <torusKnotGeometry args={size} />
       <meshStandardMaterial color={color} />
     </mesh>
   )
 }
 
-export default Sphere
+export default TorusKnot
